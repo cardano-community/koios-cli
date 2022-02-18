a434d810eb8ed3f2dec17c513abae1824e27ab89
Marko Kungla <marko.kungla@gmail.com>
2022-02-19 00:49:12 +0200

    dep: upgrade koios-go-client
    Signed-off-by: Marko Kungla <marko.kungla@gmail.com>

--- cmd-api.go
+++ cmd-api.go
@@ -749,7 +749,7 @@ func attachAPITransactionsCommmands(apicmd *cli.Command, api *koios.Client) {
 
 				stx := koios.TxBodyJSON{}
 
-				txfile, err := ioutil.ReadFile(ctx.Args().Get(0))
+				txfile, err := io.ReadFile(ctx.Args().Get(0))
 				if err != nil {
 					return err
 				}
