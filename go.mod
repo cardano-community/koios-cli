8a7ea632e94edf83fa8d7b0d95b846db827eb08f
Marko Kungla <marko@mkungla.dev>
2024-02-26 19:07:54 +0200

    fix: update main module entry
    Signed-off-by: Marko Kungla <marko@mkungla.dev>

--- go.mod
+++ go.mod
@@ -1,17 +1,26 @@
 module github.com/cardano-community/koios-cli
 
-go 1.18
+go 1.22rc2
+
+toolchain go1.22.0
 
 require (
-	github.com/cardano-community/koios-go-client/v3 v3.1.3
-	github.com/urfave/cli/v2 v2.27.1
+	github.com/cardano-community/koios-go-client/v4 v4.0.0-20240226154756-453fefc7a2a0
+	github.com/happy-sdk/happy v0.19.0
+	github.com/happy-sdk/happy/pkg/branding v0.0.0-20240226165458-fc43947ea904
+	github.com/happy-sdk/happy/pkg/cli/ansicolor v0.1.2
+	github.com/happy-sdk/happy/pkg/vars v0.8.0
 )
 
 require (
-	github.com/cpuguy83/go-md2man/v2 v2.0.3 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/happy-sdk/happy/pkg/scheduling/cron v0.3.3 // indirect
+	github.com/happy-sdk/happy/pkg/strings/bexp v1.3.2 // indirect
+	github.com/happy-sdk/happy/pkg/strings/humanize v0.1.0 // indirect
+	github.com/happy-sdk/happy/pkg/strings/textfmt v0.1.0 // indirect
+	github.com/happy-sdk/happy/pkg/version v0.1.1 // indirect
 	github.com/shopspring/decimal v1.3.1 // indirect
-	github.com/xrash/smetrics v0.0.0-20231213231151-1d8dd44e695e // indirect
+	golang.org/x/mod v0.15.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,120 +1,34 @@
-// Copyright 2022 The Cardano Community Authors
 // SPDX-License-Identifier: Apache-2.0
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at:
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//   or LICENSE file in repository root.
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright © 2024 The Cardano Community Authors
 
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"os"
-	"os/signal"
-	"runtime/debug"
-
-	"github.com/urfave/cli/v2"
-
-	"github.com/cardano-community/koios-go-client/v3"
+	"github.com/cardano-community/koios-cli/internal/api"
+	"github.com/cardano-community/koios-cli/koios"
+	"github.com/happy-sdk/happy"
+	"github.com/happy-sdk/happy/sdk/logging"
 )
 
-// nolint: gochecknoglobals
-var (
-	ErrCommand = errors.New("command error")
-
-	callctx context.Context
-	cancel  context.CancelFunc
-
-	// Populated by goreleaser during build.
-	version = "dev"
-	date    = ""
-
-	//nolint: gochecknoglobals
-	api  *koios.Client
-	opts *koios.RequestOptions
-)
-
-//nolint:gochecknoinits
-func init() {
-	if info, available := debug.ReadBuildInfo(); available && version == "dev" && date == "" {
-		version = info.Main.Version
-	}
-}
-
-// application main.
 func main() {
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	callctx, cancel = context.WithCancel(context.Background())
-	go func() {
-		<-c
-		cancel()
-	}()
+	logOpts := logging.ConsoleDefaultOptions()
+	logOpts.Level = logging.LevelOk
+	logOpts.AddSource = false
+	app := happy.New(happy.Settings{
+		Name:           "KOIOS CLI",
+		Slug:           "koios",
+		Description:    "Koios is a distributed & open-source public API query layer for Cardano, that is elastic in nature and addresses ever-demanding requirements from Cardano Blockchain. It provides a easy to query RESTful layer that has a lot of flexibility to cater for different data consumption requirements from blockchain.",
+		CopyrightBy:    "The Cardano Community",
+		CopyrightSince: 2022,
+		License:        "Apache-2.0",
+		TimeLocation:   "Local",
+		StatsEnabled:   false,
+	}).
+		WithLogger(logging.Console(logOpts)).
+		WithBrand(koios.Brand())
 
-	app := cli.NewApp()
-	app.Version = version
-	app.Authors = []*cli.Author{
-		{
-			Name: "The Cardano Community Authors",
-		},
-	}
-	app.Copyright = "(c) 2022 The Cardano Community"
-	app.Usage = "Koios CLI Client"
-	app.EnableBashCompletion = true
+	app.WithCommand(api.Command())
 
-	attachAPICommmand(app)
-	handleErr(app.Run(os.Args))
-}
-
-// common error handler.
-func handleErr(err error) {
-	if err == nil {
-		return
-	}
-	cancel()
-	// trace := err
-	// for errors.Unwrap(trace) != nil {
-	// 	trace = errors.Unwrap(trace)
-	// 	log.Println(trace)
-	// }
-	// log.Fatal(err)
-}
-
-// helper to print json response body.
-// func printJSON(ctx *cli.Context, body []byte) {
-// 	if ctx.Bool("no-format") {
-// 		fmt.Println(string(body))
-// 		return
-// 	}
-// 	var pretty bytes.Buffer
-// 	handleErr(json.Indent(&pretty, body, "", "    "))
-// 	fmt.Println(pretty.String())
-// }
-
-// output koios api client responses.
-func apiOutput(ctx *cli.Context, data interface{}, err error) {
-	handleErr(err)
-
-	if ctx.Bool("no-format") {
-		out, err := json.Marshal(data)
-		handleErr(err)
-		fmt.Println(string(out))
-		return
-	}
-
-	out, merr := json.MarshalIndent(data, "", " ")
-	handleErr(merr)
-	fmt.Println(string(out))
+	app.Run()
 }
