4dfe54f3ef7854d37f2e4f4a303a6930a5f58c5d
Marko Kungla <marko@mkungla.dev>
2024-03-07 22:44:24 +0200

    feat: implement v1 address_info
    Signed-off-by: Marko Kungla <marko@mkungla.dev>

--- internal/api/address.go
+++ internal/api/address.go
@@ -4,7 +4,14 @@
 
 package api
 
-import "github.com/happy-sdk/happy"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/cardano-community/koios-go-client/v4"
+	"github.com/happy-sdk/happy"
+	"github.com/happy-sdk/happy/pkg/vars/varflag"
+)
 
 const categoryAddress = "address"
 
@@ -21,7 +28,37 @@ func address(cmd *happy.Command, c *client) {
 }
 
 func cmdAddressAddressInfo(c *client) *happy.Command {
-	return notimplCmd(categoryAddress, "address_info")
+	cmd := happy.NewCommand("address_info",
+		happy.Option("description", "Address Information"),
+		happy.Option("category", categoryAddress),
+		happy.Option("argn.max", 100),
+	).WithFalgs(queryFlag)
+	cmd.AddInfo("Get address info - balance, associated stake address (if any) and UTxO set for given addresses")
+	cmd.AddInfo(`
+  Docs: https://api.koios.rest/#post-/address_info`)
+
+	cmd.AddFlag(varflag.StringFunc("addresses", "", "Comma separated list of addresses to query", "a"))
+	cmd.Do(func(sess *happy.Session, args happy.Args) error {
+		if !args.Flag("addresses").Present() {
+			return fmt.Errorf("missing required flag: addresses")
+		}
+
+		addrs := strings.Split(args.Flag("addresses").String(), ",")
+
+		opts, err := c.newRequestOpts(sess, args)
+		if err != nil {
+			return err
+		}
+		var addresses []koios.Address
+		for _, addr := range addrs {
+			addresses = append(addresses, koios.Address(addr))
+		}
+		res, err := c.koios().GetAddressesInfo(sess, addresses, opts)
+		apiOutput(c.noFormat, res, err)
+		return nil
+	})
+
+	return cmd
 }
 
 func cmdAddressAddressUtxos(c *client) *happy.Command {
