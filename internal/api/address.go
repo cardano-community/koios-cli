e820a63c02f93ef986ce5d2c59d49ff21cc00e68
Marko Kungla <marko@mkungla.dev>
2024-03-08 15:46:31 +0200

    fix: update address assets schema
    Signed-off-by: Marko Kungla <marko@mkungla.dev>

--- internal/api/address.go
+++ internal/api/address.go
@@ -6,11 +6,9 @@ package api
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/cardano-community/koios-go-client/v4"
 	"github.com/happy-sdk/happy"
-	"github.com/happy-sdk/happy/pkg/vars/varflag"
 )
 
 const categoryAddress = "address"
@@ -87,26 +85,33 @@ func cmdAddressAddressAssets(c *client) *happy.Command {
 	cmd := happy.NewCommand("address_assets",
 		happy.Option("description", "Address Assets"),
 		happy.Option("category", categoryAddress),
+		happy.Option("argn.max", 100),
+		happy.Option("usage", "koios api address_assets [_addresses...] // max 100"),
 	).WithFalgs(queryFlag)
 	cmd.AddInfo("Get the list of all the assets (policy, name and quantity) for given addresses")
 	cmd.AddInfo(`
-Docs: https://api.koios.rest/#post-/address_assets`)
+Docs: https://api.koios.rest/#post-/address_assets
 
-	cmd.AddFlag(varflag.StringFunc("addresses", "", "Comma separated list of addresses to query", "a"))
+_addresses query parameter is constructed from command line arguments,
+
+Example: koios-cli api address_assets \
+  addr1qy2jt0qpqz2z2z9zx5w4xemekkce7yderz53kjue53lpqv90lkfa9sgrfjuz6uvt4uqtrqhl2kj0a9lnr9ndzutx32gqleeckv \
+  addr1q9xvgr4ehvu5k5tmaly7ugpnvekpqvnxj8xy50pa7kyetlnhel389pa4rnq6fmkzwsaynmw0mnldhlmchn2sfd589fgsz9dd0y
+
+`)
+
 	cmd.Do(func(sess *happy.Session, args happy.Args) error {
-		if !args.Flag("addresses").Present() {
-			return fmt.Errorf("missing required flag: addresses")
+		if args.Argn() == 0 {
+			return fmt.Errorf("atleast one address required")
 		}
-
-		addrs := strings.Split(args.Flag("addresses").String(), ",")
 
 		opts, err := c.newRequestOpts(sess, args)
 		if err != nil {
 			return err
 		}
 		var addresses []koios.Address
-		for _, addr := range addrs {
-			addresses = append(addresses, koios.Address(addr))
+		for _, addr := range args.Args() {
+			addresses = append(addresses, koios.Address(addr.String()))
 		}
 		res, err := c.koios().GetAddressesAssets(sess, addresses, opts)
 		apiOutput(c.noFormat, res, err)
