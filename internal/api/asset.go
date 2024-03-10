62c866530181aa2d3e550493bf0cd06241ac1e36
Marko Kungla <marko@mkungla.dev>
2024-03-10 10:29:12 +0200

    fix: update asset_list command
    Signed-off-by: Marko Kungla <marko@mkungla.dev>

--- internal/api/asset.go
+++ internal/api/asset.go
@@ -12,50 +12,87 @@ const categoryAsset = "asset"
 // https://api.koios.rest/#tag--Asset
 func asset(cmd *happy.Command, c *client) {
 	cmd.DescribeCategory(categoryAsset, "Query Asset related informations")
-	cmd.AddSubCommand(cmdAssetAssetList(c))
-	cmd.AddSubCommand(cmdAssetPolicyAssetList(c))
-	cmd.AddSubCommand(cmdAssetAssetTokenRegistry(c))
-	cmd.AddSubCommand(cmdAssetAssetInfo(c))
-	cmd.AddSubCommand(cmdAssetAssetUtxos(c))
-	cmd.AddSubCommand(cmdAssetAssetHistory(c))
-	cmd.AddSubCommand(cmdAssetAssetAddresses(c))
-	cmd.AddSubCommand(cmdAssetAssetNftAddress(c))
+
+	cmd.AddSubCommand(cmdAssetAddresses(c))
+	cmd.AddSubCommand(cmdAssetHistory(c))
+	cmd.AddSubCommand(cmdAssetInfo(c))
+	cmd.AddSubCommand(cmdAssetList(c))
+	cmd.AddSubCommand(cmdAssetNftAddress(c))
+	cmd.AddSubCommand(cmdAssetSummary(c))
+	cmd.AddSubCommand(cmdAssetTokenRegistry(c))
+	cmd.AddSubCommand(cmdAssetTxs(c))
+	cmd.AddSubCommand(cmdAssetUtxos(c))
 	cmd.AddSubCommand(cmdAssetPolicyAssetAddresses(c))
 	cmd.AddSubCommand(cmdAssetPolicyAssetInfo(c))
-	cmd.AddSubCommand(cmdAssetAssetSummary(c))
-	cmd.AddSubCommand(cmdAssetAssetTxs(c))
+	cmd.AddSubCommand(cmdAssetPolicyAssetList(c))
 }
 
-func cmdAssetAssetList(c *client) *happy.Command {
-	return notimplCmd(categoryAsset, "asset_list")
-}
-
-func cmdAssetPolicyAssetList(c *client) *happy.Command {
-	return notimplCmd(categoryAsset, "policy_asset_list")
-}
-
-func cmdAssetAssetTokenRegistry(c *client) *happy.Command {
-	return notimplCmd(categoryAsset, "asset_token_registry")
-}
-
-func cmdAssetAssetInfo(c *client) *happy.Command {
-	return notimplCmd(categoryAsset, "asset_info")
-}
-
-func cmdAssetAssetUtxos(c *client) *happy.Command {
-	return notimplCmd(categoryAsset, "asset_utxos")
-}
-
-func cmdAssetAssetHistory(c *client) *happy.Command {
-	return notimplCmd(categoryAsset, "asset_history")
-}
-
-func cmdAssetAssetAddresses(c *client) *happy.Command {
+func cmdAssetAddresses(c *client) *happy.Command {
 	return notimplCmd(categoryAsset, "asset_addresses")
 }
 
-func cmdAssetAssetNftAddress(c *client) *happy.Command {
+func cmdAssetHistory(c *client) *happy.Command {
+	return notimplCmd(categoryAsset, "asset_history")
+}
+
+func cmdAssetInfo(c *client) *happy.Command {
+	return notimplCmd(categoryAsset, "asset_info")
+}
+
+func cmdAssetList(c *client) *happy.Command {
+	cmd := happy.NewCommand("asset_list",
+		happy.Option("description", "Asset List"),
+		happy.Option("category", categoryAsset),
+	).WithFalgs(pagingFlags...)
+	cmd.AddInfo("Get the list of all native assets (paginated)")
+	cmd.AddInfo(`
+  Docs: https://api.koios.rest/#get-/asset
+
+  Example: koios-cli api asset asset_list
+    {
+      ...
+      "data": [
+        {
+          "asset": {
+            "asset_name": "6e7574636f696e",
+            "policy_id": "00000002df633853f6a47465c9496721d2d5b1291b8398016c0e87ae",
+            "fingerprint": "asset12h3p5l3nd5y26lr22am7y7ga3vxghkhf57zkhd"
+          }
+        }
+      ]
+    }
+  `)
+
+	cmd.Do(func(sess *happy.Session, args happy.Args) error {
+		opts, err := c.newRequestOpts(sess, args)
+		if err != nil {
+			return err
+		}
+		res, err := c.koios().GetAssets(sess, opts)
+		apiOutput(c.noFormat, res, err)
+		return err
+	})
+	return cmd
+}
+
+func cmdAssetNftAddress(c *client) *happy.Command {
 	return notimplCmd(categoryAsset, "asset_nft_address")
+}
+
+func cmdAssetSummary(c *client) *happy.Command {
+	return notimplCmd(categoryAsset, "asset_summary")
+}
+
+func cmdAssetTokenRegistry(c *client) *happy.Command {
+	return notimplCmd(categoryAsset, "asset_token_registry")
+}
+
+func cmdAssetTxs(c *client) *happy.Command {
+	return notimplCmd(categoryAsset, "asset_txs")
+}
+
+func cmdAssetUtxos(c *client) *happy.Command {
+	return notimplCmd(categoryAsset, "asset_utxos")
 }
 
 func cmdAssetPolicyAssetAddresses(c *client) *happy.Command {
@@ -66,10 +103,6 @@ func cmdAssetPolicyAssetInfo(c *client) *happy.Command {
 	return notimplCmd(categoryAsset, "policy_asset_info")
 }
 
-func cmdAssetAssetSummary(c *client) *happy.Command {
-	return notimplCmd(categoryAsset, "asset_summary")
-}
-
-func cmdAssetAssetTxs(c *client) *happy.Command {
-	return notimplCmd(categoryAsset, "asset_txs")
+func cmdAssetPolicyAssetList(c *client) *happy.Command {
+	return notimplCmd(categoryAsset, "policy_asset_list")
 }
