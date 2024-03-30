f1b090630db43995afc0e1d5bd3085957bcb6b4d
Marko Kungla <marko@mkungla.dev>
2024-03-30 13:06:11 +0200

    sfeat: add script_info cmd

--- internal/api/script.go
+++ internal/api/script.go
@@ -4,7 +4,10 @@
 
 package api
 
-import "github.com/happy-sdk/happy"
+import (
+	"github.com/cardano-community/koios-go-client/v4"
+	"github.com/happy-sdk/happy"
+)
 
 const categoryScript = "script"
 
@@ -21,7 +24,41 @@ func script(cmd *happy.Command, c *client) {
 }
 
 func cmdScriptScriptInfo(c *client) *happy.Command {
-	return notimplCmd(categoryScript, "script_info")
+	cmd := happy.NewCommand("script_info",
+		happy.Option("description", "Script Information"),
+		happy.Option("category", categoryScript),
+		happy.Option("argn.min", 1),
+		happy.Option("argn.max", 50),
+		happy.Option("usage", "koios api script_info [_script_hashes...] // max 50"),
+	).WithFlags(pagingFlags...)
+
+	cmd.AddInfo("List of script information for given script hashes")
+
+	cmd.AddInfo(`
+    Docs: https://api.koios.rest/#post-/script_info
+
+    Example: koios-cli api script_info \
+      bd2119ee2bfb8c8d7c427e8af3c35d537534281e09e23013bca5b138 \
+      c0c671fba483641a71bb92d3a8b7c52c90bf1c01e2b83116ad7d4536
+  `)
+
+	cmd.Do(func(sess *happy.Session, args happy.Args) error {
+		opts, err := c.newRequestOpts(sess, args)
+		if err != nil {
+			return err
+		}
+
+		var hashes []koios.ScriptHash
+		for _, arg := range args.Args() {
+			hashes = append(hashes, koios.ScriptHash(arg.String()))
+		}
+
+		res, err := c.koios().GetScriptInfo(sess, hashes, opts)
+		apiOutput(c.noFormat, res, err)
+		return err
+	})
+
+	return cmd
 }
 
 func cmdScriptNativeScriptList(c *client) *happy.Command {
