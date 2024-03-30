5a737ced0a3bb73562989be977913904a5d79f7b
Marko Kungla <marko@mkungla.dev>
2024-03-30 16:29:24 +0200

    fix: update tx_info cmd
    Signed-off-by: Marko Kungla <marko@mkungla.dev>

--- internal/api/transactions.go
+++ internal/api/transactions.go
@@ -72,7 +72,45 @@ func cmdTransactionsUtxoInfo(c *client) *happy.Command {
 }
 
 func cmdTransactionsTxInfo(c *client) *happy.Command {
-	return notimplCmd(categoryTransactions, "tx_info")
+	cmd := happy.NewCommand("tx_info",
+		happy.Option("description", "Transaction Information"),
+		happy.Option("category", categoryTransactions),
+		happy.Option("argn.min", 1),
+		happy.Option("argn.max", 50),
+		happy.Option("usage", "koios api tx_info [_tx_hashes...] // max 50"),
+	).WithFlags(pagingFlags...)
+
+	cmd.AddInfo("Get detailed information about transaction(s)")
+
+	cmd.AddInfo(`
+    Docs: https://api.koios.rest/#post-/tx_info
+
+    Example: koios-cli api tx_info \
+      f144a8264acf4bdfe2e1241170969c930d64ab6b0996a4a45237b623f1dd670e \
+      0b8ba3bed976fa4913f19adc9f6dd9063138db5b4dd29cecde369456b5155e94
+
+    Example: koios-cli api tx_info --page 1 --page-size 5 \
+      f144a8264acf4bdfe2e1241170969c930d64ab6b0996a4a45237b623f1dd670e \
+      0b8ba3bed976fa4913f19adc9f6dd9063138db5b4dd29cecde369456b5155e94
+  `)
+
+	cmd.Do(func(sess *happy.Session, args happy.Args) error {
+		opts, err := c.newRequestOpts(sess, args)
+		if err != nil {
+			return err
+		}
+
+		var txs []koios.TxHash
+		for _, arg := range args.Args() {
+			txs = append(txs, koios.TxHash(arg.String()))
+		}
+
+		res, err := c.koios().GetTxInfo(sess, txs, opts)
+		apiOutput(c.noFormat, res, err)
+		return err
+	})
+
+	return cmd
 }
 
 func cmdTransactionsTxMetadata(c *client) *happy.Command {
