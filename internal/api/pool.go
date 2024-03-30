fea28958235fec26a8fffaaf2c065db07d266ea0
Marko Kungla <marko@mkungla.dev>
2024-03-30 12:17:21 +0200

    fix: update pool_blocks cmd
    Signed-off-by: Marko Kungla <marko@mkungla.dev>

--- internal/api/pool.go
+++ internal/api/pool.go
@@ -5,6 +5,8 @@
 package api
 
 import (
+	"slices"
+
 	"github.com/cardano-community/koios-go-client/v4"
 	"github.com/happy-sdk/happy"
 )
@@ -31,11 +33,11 @@ func pool(cmd *happy.Command, c *client) {
 
 func cmdPoolPoolList(c *client) *happy.Command {
 	cmd := happy.NewCommand("pool_list",
-		happy.Option("description", "List of brief info for all pools"),
+		happy.Option("description", "Pool List"),
 		happy.Option("category", categoryPool),
 	).WithFlags(pagingFlags...)
 
-	cmd.AddInfo("Get the list of all pools")
+	cmd.AddInfo("List of brief info for all pools")
 
 	cmd.AddInfo(`
     Docs: https://api.koios.rest/#get-/pool_list
@@ -160,11 +162,69 @@ func cmdPoolPoolDelegators(c *client) *happy.Command {
 }
 
 func cmdPoolPoolDelegatorsHistory(c *client) *happy.Command {
-	return notimplCmd(categoryPool, "pool_delegators_history")
+	cmd := happy.NewCommand("pool_delegators_history",
+		happy.Option("description", "Pool Delegators History"),
+		happy.Option("category", categoryPool),
+		happy.Option("argn.min", 1),
+		happy.Option("argn.max", 1),
+		happy.Option("usage", "koios api pool_delegators_history [_pool_bech32]"),
+	).WithFlags(slices.Concat(pagingFlags, flagSlice(epochNoFlag))...)
+
+	cmd.AddInfo("Return information about active delegators (incl. history) for a given pool and epoch number (all epochs if not specified).")
+
+	cmd.AddInfo(`
+    Docs: https://api.koios.rest/#get-/pool_delegators_history
+
+    Example: koios-cli api pool_delegators_history pool155efqn9xpcf73pphkk88cmlkdwx4ulkg606tne970qswczg3asc
+    Example: koios-cli api pool_delegators_history pool155efqn9xpcf73pphkk88cmlkdwx4ulkg606tne970qswczg3asc --epoch 320
+
+    `)
+
+	cmd.Do(func(sess *happy.Session, args happy.Args) error {
+		opts, err := c.newRequestOpts(sess, args)
+		if err != nil {
+			return err
+		}
+
+		res, err := c.koios().GetPoolDelegatorsHistory(sess, koios.PoolID(args.Arg(0).String()), koios.EpochNo(args.Flag("epoch").Var().Uint()), opts)
+		apiOutput(c.noFormat, res, err)
+		return err
+	})
+
+	return cmd
 }
 
 func cmdPoolPoolBlocks(c *client) *happy.Command {
-	return notimplCmd(categoryPool, "pool_blocks")
+	cmd := happy.NewCommand("pool_blocks",
+		happy.Option("description", "Pool Blocks"),
+		happy.Option("category", categoryPool),
+		happy.Option("argn.min", 1),
+		happy.Option("argn.max", 1),
+		happy.Option("usage", "koios api pool_blocks [_pool_bech32]"),
+	).WithFlags(slices.Concat(pagingFlags, flagSlice(epochNoFlag))...)
+
+	cmd.AddInfo("Return information about blocks minted by a given pool for all epochs (or _epoch_no if provided)")
+
+	cmd.AddInfo(`
+    Docs: https://api.koios.rest/#get-/pool_blocks
+
+    Example: koios-cli api pool_blocks pool155efqn9xpcf73pphkk88cmlkdwx4ulkg606tne970qswczg3asc
+    Example: koios-cli api pool_blocks pool155efqn9xpcf73pphkk88cmlkdwx4ulkg606tne970qswczg3asc --epoch 320
+
+    `)
+
+	cmd.Do(func(sess *happy.Session, args happy.Args) error {
+		opts, err := c.newRequestOpts(sess, args)
+		if err != nil {
+			return err
+		}
+
+		res, err := c.koios().GetPoolBlocks(sess, koios.PoolID(args.Arg(0).String()), koios.EpochNo(args.Flag("epoch").Var().Uint()), opts)
+		apiOutput(c.noFormat, res, err)
+		return err
+	})
+
+	return cmd
 }
 
 func cmdPoolPoolHistory(c *client) *happy.Command {
